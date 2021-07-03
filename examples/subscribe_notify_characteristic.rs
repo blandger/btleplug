68d546defac9597caf874e40edd7dab60b4df831
Andrew Walbran <qwandor@google.com>
2021-07-03 17:17:15 +0100

    Cleaning up examples a bit.

--- examples/subscribe_notify_characteristic.rs
+++ examples/subscribe_notify_characteristic.rs
@@ -1,16 +1,16 @@
 use std::time::Duration;
 
-use btleplug::api::{ValueNotification, CharPropFlags};
 use btleplug::api::{Central, Manager as _, Peripheral};
+use btleplug::api::{CharPropFlags, ValueNotification};
 use btleplug::platform::{Adapter, Manager};
-use std::io::Cursor;
+use futures::stream::StreamExt;
 use std::error::Error;
-use uuid::Uuid;
+use std::io::Cursor;
 use tokio::time;
-use futures::stream::{StreamExt};
+use uuid::Uuid;
 
 const PERIPHERAL_NAME_MATCH_FILTER: &'static str = "Neuro"; // filter BLE device by partial name
-const NOTIFY_CHARACTERISTIC_UUID:Uuid = Uuid::from_u128(0x6e400002_b534_f393_67a9_e50e24dccA9e); // subscribe UUID
+const NOTIFY_CHARACTERISTIC_UUID: Uuid = Uuid::from_u128(0x6e400002_b534_f393_67a9_e50e24dccA9e); // subscribe UUID
 
 #[cfg(target_os = "linux")]
 fn print_adapter_info(adapter: &Adapter) {
@@ -27,15 +27,8 @@ fn my_on_notification_handler(data: ValueNotification) {
     println!("Received data from [{:?}] = {:?}", data.uuid, rdr);
 }
 
-/**
-If you are getting run time error like that :
- thread 'main' panicked at 'Can't scan BLE adapter for connected devices...: PermissionDenied',
- you can try to run app with > sudo ./discover_adapters_peripherals
- on linux
-**/
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
-
     let manager = Manager::new().await?;
     let adapter_list = manager.adapters().await?;
     if adapter_list.len() <= 0 {
@@ -59,43 +52,45 @@ async fn main() -> Result<(), Box<dyn Error>> {
                 for peripheral in peripherals.iter() {
                     let properties = peripheral.properties().await?;
                     let is_connected = peripheral.is_connected().await?;
-                    let local_name = properties.local_name.unwrap_or(String::from("Unknown prop name"));
-                    println!("peripheral : {:?} is connected: {:?}",
-                             &local_name, is_connected);
+                    let local_name = properties
+                        .local_name
+                        .unwrap_or(String::from("(peripheral name unknown)"));
+                    println!(
+                        "Peripheral {:?} is connected: {:?}",
+                        &local_name, is_connected
+                    );
                     // filter needed peripheral
                     if local_name.contains(PERIPHERAL_NAME_MATCH_FILTER) {
-                        println!("found matching peripheral : {:?}...", &local_name);
-                        if !is_connected { // we can be connected previously
-                            let connect_result = peripheral
-                                .connect()
-                                .await;
-                            match connect_result {
-                                Ok(_) => {}
-                                Err(err) => {
-                                    eprintln!("Can't connect to peripheral, skipping due to error = {:?}...", err);
-                                    continue;
-                                }
+                        println!("Found matching peripheral : {:?}...", &local_name);
+                        if !is_connected {
+                            // we can be connected previously
+                            if let Err(err) = peripheral.connect().await {
+                                eprintln!(
+                                    "Can't connect to peripheral, skipping due to error = {:?}...",
+                                    err
+                                );
+                                continue;
                             }
                         }
                         let is_connected = peripheral.is_connected().await?;
                         println!(
-                            "now connected (\'{:?}\') to peripheral : {:?}...",
+                            "Now connected ({:?}) to peripheral {:?}...",
                             is_connected, &local_name
                         );
                         let chars = peripheral.discover_characteristics().await?;
                         if is_connected {
-                            println!(
-                                "Discover peripheral : \'{:?}\' characteristics...",
-                                local_name
-                            );
+                            println!("Discover peripheral {:?} characteristics...", local_name);
                             for char_item in chars.into_iter() {
-                                println!("Checking CHARACTERISTIC...: {:?} result = {:?}", char_item.uuid,
-                                         char_item.uuid == NOTIFY_CHARACTERISTIC_UUID);
+                                println!(
+                                    "Checking characteristic {:?} result = {:?}",
+                                    char_item.uuid,
+                                    char_item.uuid == NOTIFY_CHARACTERISTIC_UUID
+                                );
                                 // subscribe on selected uuid
                                 if char_item.uuid == NOTIFY_CHARACTERISTIC_UUID
-                                    && char_item.properties == CharPropFlags::NOTIFY {
-
-                                    println!("Lets try subscribe to desired CHARACTERISTIC...: {:?}", char_item.uuid);
+                                    && char_item.properties == CharPropFlags::NOTIFY
+                                {
+                                    println!("Subscribing to characteristic {:?}", char_item.uuid);
                                     peripheral.subscribe(&char_item).await?;
                                     let mut notify_result = peripheral.notifications().await?;
                                     // process while BLE connection is not broken or stopped
@@ -104,15 +99,14 @@ async fn main() -> Result<(), Box<dyn Error>> {
                                     }
                                 }
                             }
-                            println!("disconnecting from peripheral : {:?}...", local_name);
+                            println!("Disconnecting from peripheral {:?}...", local_name);
                             peripheral
                                 .disconnect()
                                 .await
-                                .expect("Error on disconnecting from BLE peripheral ");
+                                .expect("Error  disconnecting from BLE peripheral ");
                         }
                     } else {
-                        //sometimes peripheral is not discovered completely
-                        eprintln!("SKIP connect to UNKNOWN peripheral : {:?}", peripheral);
+                        println!("Skipping unknown peripheral {:?}", peripheral);
                     }
                 }
             }
